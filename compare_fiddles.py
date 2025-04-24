0583cb88a1502bf65d4568e160e2223862ea3d11
ChrisMzz <christopher.mazzerbo@gmail.com>
2025-04-24 16:47:18 +0200

    add index to dedicated branch

--- compare_fiddles.py
+++
@@ -1,7 +0,0 @@
-from fiddletools import FiddleComparator, SETTINGS
-
-comparator = FiddleComparator()
-if SETTINGS["comparator_settings"]["dump_json"]:
-    comparator.to_json()
-if SETTINGS["comparator_settings"]["dump_html"]:
-    comparator.to_html()
--- fiddle_to_html.py
+++
@@ -1,4 +0,0 @@
-from fiddletools import FiddleParser
-
-fiddle = FiddleParser(dark_mode=True)
-fiddle.to_html()
